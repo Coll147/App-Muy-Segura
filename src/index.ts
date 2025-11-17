11c3a8a003794f0b4ec33b5469ab6e19edb86478
Coll147 <aaroncoll147@gmail.com>
2025-11-17 09:00:24 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express, { Application, Request, Response} from "express"
+
+const app: Application = express()
+const port: number = 3000
+
+// instalar motor de vistas 
+app.set('view engine', 'ejs')
+// definir carpeta
+app.set('views', './src/views')
+
+// endpoint raiz
+app.get('/', (req: Request, res: Response) => {
+  res.render('index', { message: 'Indeeeeex' })
+})
+
+// endpoint raiz
+app.get('/login', (req: Request, res: Response) => {
+  res.render('login', { message: 'Login Page' })
+})
+
+
+// endpoint cat
+app.get('/cat', (req: Request, res: Response) => {
+    res.send('<h1>Gato.</h1> <img style="height: 500px" src="https://preview.redd.it/get-server-rack-to-stop-cat-messing-with-my-computers-cat-v0-9sjf2dtnbvxd1.jpeg?width=1080&crop=smart&auto=webp&s=730a503c84234fa69ea82fd144a9b90231726ee9">')
+})
+
+// iniciar el servidor
+app.listen(port, () => {
+  console.log(`Link for app http://localhost:${port}`)
+})
